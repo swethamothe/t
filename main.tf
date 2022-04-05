d75299ce1a50ec4bedb47d3907d7e97eec46e1f0
swethamothe <89130824+swethamothe@users.noreply.github.com>
2022-04-05 16:13:47 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      
+      #version = ">= 3.20.0"
+    }
+  
+  }	  
+}	  
+provider "aws" {
+	region  = local.workspace["vpc_region"]
+}
+
+module "ec2vpc" {
+    source = "./"
+
+      vpc_region  = local.workspace["vpc_region"]
+      env_name = local.workspace["env_name"]
+      project_name  = local.workspace["project_name"]
+      ami = local.workspace["ami"]
+      vpc = local.workspace["vpc"]
+      subnet = local.workspace["subnet"]
+      instance_type = local.workspace["instance_type"]
+}
