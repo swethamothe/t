2243806634a8685289933b2f95feb4f18c43997b
swethamothe <89130824+swethamothe@users.noreply.github.com>
2022-04-05 16:29:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "ec2" {
       vpc_region  = local.workspace["vpc_region"]
       env_name = local.workspace["env_name"]
       project_name  = local.workspace["project_name"]
-      ami = local.workspace["ami"]
+      ami = local.workspace["myami"]
       vpc = local.workspace["vpc"]
       subnet = local.workspace["subnet"]
       instance_type = local.workspace["instance_type"]
